6ca210eba3e4ae9e2f9518b7fee09fb9e1a451db
Dilpesh786 <onlineerning33@gmail.com>
2026-09-24 21:53:55 +0530

    Create requestLogger.js

---
+++ middleware/requestLogger.js
@@ -0,0 +1,6 @@
+const requestLogger = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} request to ${req.originalUrl}`);
+    next();
+};
+
+module.exports = requestLogger;
