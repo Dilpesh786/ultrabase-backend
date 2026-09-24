7ac2450bdaa367a5801004aad2e36146403a8740
Dilpesh786 <onlineerning33@gmail.com>
2026-09-24 21:47:38 +0530

    Create validators.js

---
+++ utils/validators.js
@@ -0,0 +1,13 @@
+const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+
+const isValidPassword = (password) => {
+    return password && password.length >= 6;
+};
+
+module.exports = {
+    isValidEmail,
+    isValidPassword
+};
