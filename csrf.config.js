8ee64064910f1ee8c3fa5296fca209a414eef407
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 21:31:58 +0530

    Create csrf.config.js

---
+++ csrf.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  csrf: {
+    cookie: {
+      key: '_csrf',
+      path: '/',
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict'
+    },
+    ignoreMethods: ['GET', 'HEAD', 'OPTIONS']
+  }
+};
