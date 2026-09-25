ea2a5754a2d59535349d1b0ae1ad399e52d82073
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 08:15:48 +0530

    Create session.config.js

---
+++ session.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  session: {
+    secret: process.env.SESSION_SECRET || 'ultrabase_secret_key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  }
+};
