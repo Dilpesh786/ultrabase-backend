1da78bba7125096700fd7b7afffbea4fb5c94bbd
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 08:03:54 +0530

    Create cluster.js

---
+++ cluster.js
@@ -0,0 +1,21 @@
+const cluster = require('cluster');
+const os = require('os');
+const http = require('http');
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Starting a new worker...`);
+    cluster.fork();
+  });
+} else {
+  require('./server.js');
+  console.log(`Worker ${process.pid} started`);
+}
