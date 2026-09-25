f0103dbd10a45c8a3ef6d4e65e2018ca639d474a
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 21:35:44 +0530

    Create cors.config.js

---
+++ cors.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  cors: {
+    origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    credentials: true,
+    optionsSuccessStatus: 200
+  }
+};
