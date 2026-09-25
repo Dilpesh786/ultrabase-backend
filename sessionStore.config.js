0c129073bbee4a4c63074d448ab43281d98e9fa3
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 21:29:57 +0530

    Create sessionStore.config.js

---
+++ sessionStore.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  sessionStore: {
+    secret: process.env.SESSION_SECRET || 'supersecretkey',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    }
+  }
+};
