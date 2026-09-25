8ed291ec74eb8ee0190aadd1182c6ba62c8843dd
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 08:17:29 +0530

    Create cookie.config.js

---
+++ cookie.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  cookie: {
+    signed: true,
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    sameSite: 'strict',
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  }
+};
