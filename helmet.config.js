0dc3e73e540a5abe17c8921cd17a18f8cfb55efb
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 21:33:49 +0530

    Create helmet.config.js

---
+++ helmet.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  helmet: {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        scriptSrc: ["'self'"],
+        imgSrc: ["'self'", "data:"]
+      }
+    },
+    crossOriginEmbedderPolicy: true
+  }
+};
