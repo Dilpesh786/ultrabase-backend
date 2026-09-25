2a2740a3ee95a2776382a2e2afb7adf942e0fe8a
Dilpesh786 <onlineerning33@gmail.com>
2026-09-25 21:28:10 +0530

    Create compression.config.js

---
+++ compression.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  compression: {
+    level: 6,
+    threshold: 1024,
+    filter: (req, res) => {
+      if (req.headers['x-no-compression']) {
+        return false;
+      }
+      return true;
+    }
+  }
+};
