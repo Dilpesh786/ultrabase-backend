b2146d80b6c34ceb7f017844c3b425a1b8b6d0a8
Dilpesh786 <onlineerning33@gmail.com>
2026-09-26 21:53:06 +0530

    Create maintenanceSchedule.config.js

---
+++ maintenanceSchedule.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  maintenance: {
+    enabled: false,
+    allowedIPs: ['127.0.0.1', '::1'],
+    message: 'System is under scheduled maintenance. Please check back later.'
+  }
+};
